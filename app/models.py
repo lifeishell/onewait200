2bf58d65e21b156016f30e05219e6e0fff5d2beb
Jindiying <jindiying@360.cn>
2015-09-11 20:36:23 +0800

    Add admin

--- app/models.py
+++ app/models.py
@@ -4,6 +4,6 @@ class Person(models.Model):
     name = models.CharField(max_length=30)
 
 class Relation(models.Model):
-    from_person = models.ForeignKey(Persons, related_name="from")
-    to_person = models.ForeignKey(Persons, related_name="to")
+    from_person = models.ForeignKey(Person, related_name="from")
+    to_person = models.ForeignKey(Person, related_name="to")
     description = models.CharField(max_length=256)
