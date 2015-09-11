477eb31e9324f3b58f586d5e5141a6610cedfa35
guai 11 <guai11@sallinen.dreamhost.com>
2015-09-11 05:55:46 -0700

    Add

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from onewait200.app.models import Person, Relation
 
-class RelationInline(admin.TabularInline):
-    model = Relation
+class RelationAdmin(admin.ModelAdmin):
+    list_display = ('from_person', 'to_person', 'description')
+
+admin.site.register(Relation, RelationAdmin)
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('name',)
-    inlines = [
-        RelationInline,
-    ]
+
 admin.site.register(Person, PersonAdmin)
--- app/models.py
+++ app/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+	return self.name
 
 class Relation(models.Model):
     from_person = models.ForeignKey(Person, related_name="from")
     to_person = models.ForeignKey(Person, related_name="to")
     description = models.CharField(max_length=256)
+
+    class Meta:
+        unique_together = (("from_person", "to_person"),)
